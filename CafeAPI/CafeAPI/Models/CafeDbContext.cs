2524716d705f1451f5c121c34a6a9e535b490bc2
MTATrooper <long96nb@gmail.com>
2019-06-27 07:54:24 +0700

    first

---
+++ CafeAPI/CafeAPI/Models/CafeDbContext.cs
@@ -0,0 +1,130 @@
+namespace CafeAPI.Models
+{
+    using System;
+    using System.Data.Entity;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Linq;
+
+    public partial class CafeDbContext : DbContext
+    {
+        public CafeDbContext()
+            : base("name=CafeDbContext")
+        {
+        }
+
+        public virtual DbSet<BAIBAO> BAIBAO { get; set; }
+        public virtual DbSet<BINHLUAN> BINHLUAN { get; set; }
+        public virtual DbSet<CHITIETDONHANG> CHITIETDONHANG { get; set; }
+        public virtual DbSet<CHITIETNHAPHANG> CHITIETNHAPHANG { get; set; }
+        public virtual DbSet<DONHANG> DONHANG { get; set; }
+        public virtual DbSet<HANGSX> HANGSX { get; set; }
+        public virtual DbSet<LOAISP> LOAISP { get; set; }
+        public virtual DbSet<NGUOIDUNG> NGUOIDUNG { get; set; }
+        public virtual DbSet<NHACUNGCAP> NHACUNGCAP { get; set; }
+        public virtual DbSet<NHAPHANG> NHAPHANG { get; set; }
+        public virtual DbSet<PRICE> PRICE { get; set; }
+        public virtual DbSet<SANPHAM> SANPHAM { get; set; }
+        public virtual DbSet<THONGKE> THONGKE { get; set; }
+        public virtual DbSet<TRANGTHAIDH> TRANGTHAIDH { get; set; }
+        public virtual DbSet<XUATHANG> XUATHANG { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CHITIETDONHANG>()
+                .HasMany(e => e.XUATHANG)
+                .WithOptional(e => e.CHITIETDONHANG)
+                .HasForeignKey(e => e.CHITIETDH_ID);
+
+            modelBuilder.Entity<DONHANG>()
+                .Property(e => e.SDT)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<DONHANG>()
+                .HasMany(e => e.CHITIETDONHANG)
+                .WithOptional(e => e.DONHANG)
+                .HasForeignKey(e => e.DONHANG_ID);
+
+            modelBuilder.Entity<HANGSX>()
+                .HasMany(e => e.LOAISP)
+                .WithOptional(e => e.HANGSX)
+                .HasForeignKey(e => e.HANGSX_ID);
+
+            modelBuilder.Entity<LOAISP>()
+                .HasMany(e => e.SANPHAM)
+                .WithOptional(e => e.LOAISP)
+                .HasForeignKey(e => e.LOAISP_ID);
+
+            modelBuilder.Entity<NGUOIDUNG>()
+                .Property(e => e.TENDANGNHAP)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<NGUOIDUNG>()
+                .Property(e => e.SDT)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<NGUOIDUNG>()
+                .HasMany(e => e.BINHLUAN)
+                .WithOptional(e => e.NGUOIDUNG)
+                .HasForeignKey(e => e.KHACHHANG_ID);
+
+            modelBuilder.Entity<NGUOIDUNG>()
+                .HasMany(e => e.DONHANG)
+                .WithOptional(e => e.NGUOIDUNG)
+                .HasForeignKey(e => e.KHACHHANG_ID);
+
+            modelBuilder.Entity<NHACUNGCAP>()
+                .Property(e => e.SDT)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<NHACUNGCAP>()
+                .HasMany(e => e.NHAPHANG)
+                .WithOptional(e => e.NHACUNGCAP)
+                .HasForeignKey(e => e.NHACC_ID);
+
+            modelBuilder.Entity<NHAPHANG>()
+                .HasMany(e => e.CHITIETNHAPHANG)
+                .WithOptional(e => e.NHAPHANG)
+                .HasForeignKey(e => e.NHAPHANG_ID);
+
+            modelBuilder.Entity<NHAPHANG>()
+                .HasMany(e => e.XUATHANG)
+                .WithOptional(e => e.NHAPHANG)
+                .HasForeignKey(e => e.NHAPHANG_ID);
+
+            modelBuilder.Entity<SANPHAM>()
+                .HasMany(e => e.BAIBAO)
+                .WithOptional(e => e.SANPHAM)
+                .HasForeignKey(e => e.SANPHAM_ID);
+
+            modelBuilder.Entity<SANPHAM>()
+                .HasMany(e => e.BINHLUAN)
+                .WithOptional(e => e.SANPHAM)
+                .HasForeignKey(e => e.SANPHAM_ID);
+
+            modelBuilder.Entity<SANPHAM>()
+                .HasMany(e => e.CHITIETDONHANG)
+                .WithOptional(e => e.SANPHAM)
+                .HasForeignKey(e => e.SANPHAM_ID);
+
+            modelBuilder.Entity<SANPHAM>()
+                .HasMany(e => e.CHITIETNHAPHANG)
+                .WithOptional(e => e.SANPHAM)
+                .HasForeignKey(e => e.SANPHAM_ID);
+
+            modelBuilder.Entity<SANPHAM>()
+                .HasMany(e => e.PRICE)
+                .WithOptional(e => e.SANPHAM)
+                .HasForeignKey(e => e.SANPHAM_ID);
+
+            modelBuilder.Entity<SANPHAM>()
+                .HasMany(e => e.THONGKE)
+                .WithOptional(e => e.SANPHAM)
+                .HasForeignKey(e => e.SANPHAM_ID);
+
+            modelBuilder.Entity<TRANGTHAIDH>()
+                .HasMany(e => e.DONHANG)
+                .WithOptional(e => e.TRANGTHAIDH)
+                .HasForeignKey(e => e.TRANGTHAI_ID);
+        }
+    }
+}
---
+++ CafeAPI/CafeAPI/Models/SANPHAM.cs
@@ -0,0 +1,57 @@
+namespace CafeAPI.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("SANPHAM")]
+    public partial class SANPHAM
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public SANPHAM()
+        {
+            BAIBAO = new HashSet<BAIBAO>();
+            BINHLUAN = new HashSet<BINHLUAN>();
+            CHITIETDONHANG = new HashSet<CHITIETDONHANG>();
+            CHITIETNHAPHANG = new HashSet<CHITIETNHAPHANG>();
+            PRICE = new HashSet<PRICE>();
+            THONGKE = new HashSet<THONGKE>();
+        }
+
+        public int ID { get; set; }
+
+        public int? KHOILUONG { get; set; }
+
+        [StringLength(200)]
+        public string ANH { get; set; }
+
+        [Column(TypeName = "ntext")]
+        public string MOTA { get; set; }
+
+        public int? SOLUONG { get; set; }
+
+        public int? LOAISP_ID { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<BAIBAO> BAIBAO { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<BINHLUAN> BINHLUAN { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<CHITIETDONHANG> CHITIETDONHANG { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<CHITIETNHAPHANG> CHITIETNHAPHANG { get; set; }
+
+        public virtual LOAISP LOAISP { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<PRICE> PRICE { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<THONGKE> THONGKE { get; set; }
+    }
+}
